27be74fde55ec7da7d10fe47921a4d701d923feb
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2019-07-16 08:43:31 +0200

    Publish jest config

--- packages/tdd-buffet/src/config/jest.config.js
+++ packages/tdd-buffet/src/config/jest.config.js
@@ -1,14 +1,12 @@
 const path = require('path');
 const { pathsToModuleNameMapper } = require('ts-jest/utils');
-const { compilerOptions } = require('../tsconfig');
-
-const rootDir = path.join(__dirname, '../');
+const { compilerOptions } = require(path.join(process.cwd(), 'tsconfig'));
 
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
 
-  rootDir,
+  rootDir: process.cwd(),
   testMatch: ['**/tests/**/*.spec.{ts,tsx}'],
   moduleNameMapper: {
     ...pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/packages/' })
@@ -24,7 +22,7 @@ module.exports = {
     '!**/tests/**/*',
     '!**/vendor/**/*'
   ],
-  coverageDirectory: path.join(rootDir, 'tests/results'),
+  coverageDirectory: '<rootDir>/tests/results',
   coverageReporters: ['json', 'text', 'html'],
   coverageThreshold: {
     global: {
