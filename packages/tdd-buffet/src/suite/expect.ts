59d7dea5fd9b290740063f54e4de99d8688cab70
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2019-08-20 21:38:57 +0200

    Try to fix the expect symbol not being picked up by WebStorm

--- packages/tdd-buffet/src/suite/expect.ts
+++ packages/tdd-buffet/src/suite/expect.ts
@@ -1,1 +1,5 @@
-export { expect } from 'chai';
+import { expect } from 'chai';
+
+// Exporting it this way instead of `export ... from ...` because WebStorm can't seem
+// to discover it.
+export { expect };
