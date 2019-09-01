9601c3fda8129ebb4906b817d8d2c53ed07b5685
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2019-09-01 09:56:25 +0200

    Add eslint-plugin-import

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,23 @@ module.exports = {
   'root': true,
 
   'extends': [
-    '@nighttrax/eslint-config-tsx'
-  ]
+    '@nighttrax/eslint-config-tsx',
+    'plugin:import/errors'
+  ],
+
+  'plugins': ['import'],
+
+  'rules': {
+    'import/no-extraneous-dependencies': 'error'
+  },
+
+  'settings': {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx']
+    },
+    'import/resolver': {
+      // use <root>/tsconfig.json
+      'typescript': {},
+    }
+  }
 };
