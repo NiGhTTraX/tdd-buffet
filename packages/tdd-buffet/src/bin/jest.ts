ddd063efd8083c06c247ee1d407bf6fad84b88d4
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2019-09-17 07:08:01 +0200

    Revert "Add Jest binary"
    This reverts commit 3a594e8c. It's not being picked up by WebStorm
    as expected and it's not covered by tests so removing it until I
    have a clearer understanding.

--- packages/tdd-buffet/src/bin/jest.ts
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-/**
- * This file is meant to be used by WebStorm when searching for the Jest binary.
- */
-import { run } from '../jest';
-
-(async () => {
-  await run(process.argv.slice(2));
-})();
