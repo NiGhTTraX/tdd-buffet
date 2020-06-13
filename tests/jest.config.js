a765cd00dca999b0156466d130b84fc02876d9c6
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2020-06-13 18:45:34 +0200

    chore: Simplify jest config

--- tests/jest.config.js
+++ tests/jest.config.js
@@ -2,10 +2,4 @@ const baseConfig = require('@tdd-buffet/jest-config');
 
 module.exports = {
   ...baseConfig,
-
-  collectCoverageFrom: [
-    ...baseConfig.collectCoverageFrom,
-    '!**/tests/coverage/ignored.js',
-  ],
-  forceCoverageMatch: ['**/tests/coverage/*.ts', '**/tests/coverage/*.tsx'],
 };
