5f7186186e40bf6dbff95e105fb2dec9ce054d6d
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2021-04-25 15:07:18 +0200

    test(visual): Update fixtures to remove flakiness

--- packages/visual/tests/visual.spec.ts
+++ packages/visual/tests/visual.spec.ts
@@ -1,20 +1,36 @@
-import { describe } from 'tdd-buffet/suite/gui';
+import { describe, beforeEach } from 'tdd-buffet/suite/gui';
 import { vit } from '../src/index';
 
 describe('Visual suite', () => {
-  vit('should take a screenshot of the only child', async (page) => {
-    await page.goto('about:blank');
+  beforeEach((page) => page.goto('about:blank'));
 
+  vit('should take a screenshot of the only child', async (page) => {
     await page.evaluate(function () {
-      document.body.innerHTML = '<span>screenshot</span>';
+      document.body.innerHTML = `
+<div style="width: 100px; height: 100px; background-color: red"/>
+`;
     });
   });
 
   vit('should take a screenshot of the first child', async (page) => {
-    await page.goto('about:blank');
-
     await page.evaluate(function () {
-      document.body.innerHTML = '<span>1</span><span>2</span>';
+      document.body.innerHTML = `
+<div style="width: 100px; height: 100px; background-color: green"></div>
+<div style="width: 100px; height: 100px; background-color: blue"></div>
+`;
     });
   });
+
+  vit(
+    'should take a screenshot of the second child',
+    async (page) => {
+      await page.evaluate(function () {
+        document.body.innerHTML = `
+<div style="width: 100px; height: 100px; background-color: green"/>
+<div data-testid="second" style="width: 100px; height: 100px; background-color: blue"/>
+`;
+      });
+    },
+    '[data-testid="second"]'
+  );
 });
